c0f5a8b096fea72abf19308fed63004efa39393b
Leonardo Minora <leonardo.minora@gmail.com>
2023-07-31 12:09:27 -0300

    added attributes and decorators in Tarafa

--- src/tarefas/entities/tarefa.entity.ts
+++ src/tarefas/entities/tarefa.entity.ts
@@ -1,1 +1,13 @@
-export class Tarefa {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Tarefa {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  titulo: string;
+
+  @Column({ default: false })
+  feito: boolean;
+}
